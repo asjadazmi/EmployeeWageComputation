35d7dc7f74934d48e25db87c6a510f650181a72e
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-05-26 12:25:28 +0530

    added :attendence of the employee

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -1,8 +1,17 @@
 package com.bridgelabz;
-
+import java.util.Random.*;
 public class EmployeeWageComputation {
 	public static void main(String args[]) {
 		System.out.println("welcome to Employee wage computation program");
+		int is_Full_time=1;
+		double empCheck=Math.floor(Math.random()*10)%10;
+		System.out.println(empCheck);
+		if(empCheck==is_Full_time) {
+			System.out.println("employee is present");
+		}else {
+			System.out.println("Employee is abscent");
+		}
+		
 	}
 
 }
