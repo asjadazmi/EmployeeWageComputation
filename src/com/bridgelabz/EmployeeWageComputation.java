988fb1b3e8d645d2b3c3adcc52b9150b9ffd7c15
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-05-26 13:20:13 +0530

    Added :part time

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -4,14 +4,20 @@ public class EmployeeWageComputation {
 	public static void main(String args[]) {
 		System.out.println("welcome to Employee wage computation program");
 		int is_Full_time=1;
+		int is_Part_Time=2;
 		int Emp_Rate_Per_Hour=20;
 		int emphrs=0;
 		int empwage=0;
-		double empCheck=Math.floor(Math.random()*10)%2;
+		double empCheck=Math.floor(Math.random()*10)%3;
 		if(empCheck==is_Full_time) {
-			System.out.println("Employee is present");
+			System.out.println("Employee is present full time");
 			emphrs=8;
-			}else 
+			}
+		else if(empCheck==2) {
+			System.out.println("Employee is present part time");
+			emphrs=4;
+		}
+		else 
 			{
 				System.out.println("Employee is absent");
 				emphrs=0;
