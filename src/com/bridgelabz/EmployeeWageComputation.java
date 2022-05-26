17591f90ee22ebb693ab2c3ea4db4c05f66e7f28
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-05-26 13:04:15 +0530

    Added: Daily wages of employee

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -4,14 +4,20 @@ public class EmployeeWageComputation {
 	public static void main(String args[]) {
 		System.out.println("welcome to Employee wage computation program");
 		int is_Full_time=1;
+		int Emp_Rate_Per_Hour=20;
+		int emphrs=0;
+		int empwage=0;
 		double empCheck=Math.floor(Math.random()*10)%2;
-		System.out.println(empCheck);
 		if(empCheck==is_Full_time) {
-			System.out.println("employee is present");
-		}else {
-			System.out.println("Employee is abscent");
-		}
-		
+			System.out.println("Employee is present");
+			emphrs=8;
+			}else 
+			{
+				System.out.println("Employee is absent");
+				emphrs=0;
+			}
+		empwage=emphrs*Emp_Rate_Per_Hour;
+		System.out.println("empwage: "+empwage);
 	}
 
 }
