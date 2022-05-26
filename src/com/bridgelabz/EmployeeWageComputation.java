0c2985d8f2bcd77a9636359ee4ed69cf5f1db2f0
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-05-26 12:42:18 +0530

    Added: Attendence

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -1,10 +1,10 @@
 package com.bridgelabz;
-import java.util.Random.*;
+
 public class EmployeeWageComputation {
 	public static void main(String args[]) {
 		System.out.println("welcome to Employee wage computation program");
 		int is_Full_time=1;
-		double empCheck=Math.floor(Math.random()*10)%10;
+		double empCheck=Math.floor(Math.random()*10)%2;
 		System.out.println(empCheck);
 		if(empCheck==is_Full_time) {
 			System.out.println("employee is present");
