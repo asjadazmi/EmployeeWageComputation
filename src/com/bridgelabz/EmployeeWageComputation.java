ee5783acad2eeda571babb487a8af294c41f02a8
S MD ASJAD AZMI <asjadazmi0786@gmail.com>
2022-05-26 15:02:50 +0530

    Added: using Switch statements

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -1,29 +1,30 @@
 package com.bridgelabz;
+// solving using case statement
 
 public class EmployeeWageComputation {
-	public static void main(String args[]) {
-		System.out.println("welcome to Employee wage computation program");
-		int is_Full_time=1;
-		int is_Part_Time=2;
-		int Emp_Rate_Per_Hour=20;
+	public static final int is_Full_Time=1;
+	public static final int Is_Part_Time=2;
+	public static final int Emp_Rate_Per_Hrs=20;
+	public static void main(String args[]){
 		int emphrs=0;
+		int totalemphrs=0;
 		int empwage=0;
-		double empCheck=Math.floor(Math.random()*10)%3;
-		if(empCheck==is_Full_time) {
-			System.out.println("Employee is present full time");
-			emphrs=8;
-			}
-		else if(empCheck==2) {
-			System.out.println("Employee is present part time");
-			emphrs=4;
-		}
-		else 
-			{
-				System.out.println("Employee is absent");
-				emphrs=0;
-			}
-		empwage=emphrs*Emp_Rate_Per_Hour;
-		System.out.println("empwage: "+empwage);
+		int empCheck=(int)Math.floor(Math.random()*10)%3;
+		switch (empCheck){
+		     case
+		        is_Full_Time:
+			    emphrs=8;
+			 break;
+		     case Is_Part_Time:
+		    	 emphrs=4;
+		    	 break;
+		     default:
+		    	 emphrs=0;
+		    	 }
+		totalemphrs+=emphrs;
+		int totalempwage=Emp_Rate_Per_Hrs*emphrs;
+		System.out.println("total hrs: "+totalemphrs+" totalempwage "+totalempwage);
 	}
-
-}
+	
+	}
+	
